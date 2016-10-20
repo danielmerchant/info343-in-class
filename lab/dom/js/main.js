1d2588fd496afdff406c560240c3a826809d69e8
Daniel Merchant <daniel.t.merchant@gmail.com>
2016-10-20 12:09:47 -0700

    in-class work 10/20

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,43 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+	boxOne.addEventListener("click", function() {
+		boxOne.classList.toggle("fade-to-black");
+	});
+
+	boxTwo.addEventListener("click", function() {
+		boxTwo.classList.add("fall");
+	});
+
+	boxThree.addEventListener("mouseover", function() {		
+		boxThree.classList.add("fade-to-white");
+		boxThree.classList.add("white");
+	});
+
+	boxThree.addEventListener("mouseout", function() {
+		boxThree.classList.remove("white");
+		boxThree.classList.add("fade-to-bg-cover");
+	});
+
+	boxFour.addEventListener("click", function() {
+		boxFour.classList.toggle("fall");
+	});
+
+	boxFive.addEventListener("click", function() {
+		function explode() {
+			var style = getComputedStyle(boxFive);
+			var width = parseInt(style.getPropertyValue('width'));
+			var height = parseInt(style.getPropertyValue('height'));
+			if (width > 250) {
+				boxFive.style.width = 0 + "px";
+				boxFive.innerHTML = "";
+			} else if (width != 0) {
+				width += 5;
+				height += 5;
+				boxFive.style.width = width + "px";
+				boxFive.style.height = height + "px";
+			}
+		}
+		setInterval(function(){ explode() }, 50);
+	});
