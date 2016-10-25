7f62b9e525f0349c6998fa16bc1500c292eac492
Daniel Merchant <daniel.t.merchant@gmail.com>
2016-10-25 12:17:49 -0700

    in-class exercises 10/25

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -12,3 +12,97 @@
 //  to the URL provided by Firebase 
 
 //paste the Firebase initialization code here
+
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCx1Igmro7xT0XZxLpjR9zhGgF1M-94IvA",
+    authDomain: "tasks-demo-ad2dd.firebaseapp.com",
+    databaseURL: "https://tasks-demo-ad2dd.firebaseio.com",
+    storageBucket: "tasks-demo-ad2dd.appspot.com",
+    messagingSenderId: "128979476790"
+};
+firebase.initializeApp(config);
+
+var currentUser;
+var authProvider = new firebase.auth.GithubAuthProvider();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser = user;
+        console.log(currentUser);
+    } else {
+        firebase.auth().signInWithRedirect(authProvider);
+    }
+}); 
+
+var taskForm = document.querySelector(".new-task-form");
+var taskTitleInput = taskForm.querySelector(".new-task-title");
+var taskList = document.querySelector(".task-list");
+var purgeButton = document.querySelector(".btn-purge");
+
+var tasksRef = firebase.database().ref("tasks");
+
+taskForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    var task = {
+        title: taskTitleInput.value.trim(),
+        done: false,
+        createdOn: firebase.database.ServerValue.TIMESTAMP,
+        createdBy: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email
+        }
+    };
+    tasksRef.push(task);
+    taskTitleInput.value = "";
+
+    return false;
+});
+
+function renderTask(snapshot) {
+    var task = snapshot.val();
+    var li = document.createElement("li");
+    var spanTitle = document.createElement("span");
+    spanTitle.textContent = task.title;
+    spanTitle.classList.add("task-title");
+    li.appendChild(spanTitle);
+
+    var spanCreation = document.createElement("span");
+    spanCreation.textContent = moment(task.createdOn).fromNow() + " by " +task.createdBy.displayName;
+    spanCreation.classList.add("task-creation");
+    
+    li.appendChild(spanCreation);
+
+    if (task.done) {
+        li.classList.add("done");
+        purgeButton.classList.remove("hidden");
+    }
+    
+    li.addEventListener("click", function() {
+        snapshot.ref.update({
+            done: !task.done
+        });
+    });
+
+    taskList.appendChild(li);
+}
+
+function render(snapshot) {
+    taskList.innerHTML = "";
+    purgeButton.classList.add("hidden");
+    snapshot.forEach(renderTask);
+}
+
+tasksRef.on("value", render);
+
+purgeButton.addEventListener("click", function() {
+    tasksRef.once("value", function(snapshot) {
+        snapshot.forEach(function(taskSnapshot) {
+            if (taskSnapshot.val().done) {
+                taskSnapshot.ref.remove();
+            }
+        });
+    });
+});
